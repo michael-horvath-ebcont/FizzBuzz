270a584d5bec7a9a84deb61c1bd37ca5e29f7054
michael.horvath <michael.horvath@ebcont.com>
2022-04-08 10:36:41 +0200

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/com/company/MainTest.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    /*
+    Program:
+        1. User enters a number into terminal
+        2. Program decides:
+            * number % 3 ==0: print number Fizz
+            * number % 5 ==0: print number fuzz
+            * both: print number FizzBuzz
+     */
+
+}
